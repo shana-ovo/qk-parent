b94fb4da2b1fa23adbebcdd1e4f2ecaa9f383e49
e1@ <qwzddbm@gmail.com>
2026-04-20 16:49:46 +0800

    refactor(oss): 将阿里云OSS配置外部化到配置文件
    - 移除硬编码的OSS配置常量
    - 引入AliyunOSSProperties类管理OSS配置
    - 使用@Autowired注入配置属性
    - 将OSS配置迁移到application.yaml文件
    - 支持通过配置文件动态设置endpoint、bucketName和region参数

--- qk-common/src/main/java/com/qk/utils/AliyunOSSOperator.java
+++ qk-common/src/main/java/com/qk/utils/AliyunOSSOperator.java
@@ -5,6 +5,7 @@ import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
 import com.aliyun.oss.common.comm.SignVersion;
 import com.aliyun.oss.model.PutObjectRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
@@ -12,29 +13,32 @@ import java.io.ByteArrayInputStream;
 @Slf4j
 @Component
 public class AliyunOSSOperator {
-
-    private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
-    private static final String BUCKET_NAME = "bucket-llh";
-    private static final String REGION = "cn-beijing";
+    @Autowired
+    private AliyunOSSProperties ossProperties;
 
     public String upload(byte[] content, String objectName) throws Exception {
+
+        String endpoint = ossProperties.getEndpoint();
+        String bucketName = ossProperties.getBucketName();
+        String region = ossProperties.getRegion();
+
         // 创建OSSClient实例
         ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
         clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
         OSS ossClient = null;
         try {
             ossClient = OSSClientBuilder.create()
-                    .endpoint(ENDPOINT)
+                    .endpoint(endpoint)
                     .credentialsProvider(new EnvironmentVariableCredentialsProvider())
                     .clientConfiguration(clientBuilderConfiguration)
-                    .region(REGION)
+                    .region(region)
                     .build();
             // 创建PutObjectRequest对象
-            PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME, objectName, new ByteArrayInputStream(content));
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, objectName, new ByteArrayInputStream(content));
             // 上传文件
             ossClient.putObject(putObjectRequest);
             // 返回文件访问URL
-            return "https://" + BUCKET_NAME + "." + ENDPOINT.substring(8) + "/" + objectName;
+            return "https://" + bucketName + "." + endpoint.substring(8) + "/" + objectName;
         } catch (Exception e) {
             log.error("Caught an OSSException: " + e.getMessage());
             throw e;
